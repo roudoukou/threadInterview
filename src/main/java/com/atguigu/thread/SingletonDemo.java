9de2f1702f7a5a96a18591ebb81b144b7d1b305f
肉豆蔻 <1023876294@qq.com>
2024-01-04 15:01:21 +0800

    单例模式-synchronized

---
+++ src/main/java/com/atguigu/thread/SingletonDemo.java
@@ -0,0 +1,34 @@
+package com.atguigu.thread;
+
+public class SingletonDemo {
+    private static SingletonDemo instance;
+
+    private SingletonDemo() {
+        System.out.println(Thread.currentThread().getName() + "\t构造方法SingletonDemo()" );
+    }
+
+    public synchronized static SingletonDemo getInstance() {
+        if (instance == null) {
+            instance = new SingletonDemo();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        // 单线程 (main线程的操作动作......)
+/*
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+        System.out.println(SingletonDemo.getInstance() == SingletonDemo.getInstance());
+*/
+
+        // 并发多线程后, 情况发生了很大的变化
+        for(int i = 1; i <= 10; i++) {
+            new Thread(() -> {
+                SingletonDemo.getInstance();
+            }, String.valueOf(i)).start();
+        }
+
+
+    }
+}
