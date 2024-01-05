560d5cde8d15d1e08da84c9f4c46529af073c289
肉豆蔻 <1023876294@qq.com>
2024-01-05 16:36:28 +0800

    原子引用AtomicReference

--- src/main/java/com/atguigu/thread/AtomicReferenceDemo.java
+++ src/main/java/com/atguigu/thread/AtomicReferenceDemo.java
@@ -1,10 +1,29 @@
 package com.atguigu.thread;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 class User {
     String userName;
     int age;
 }
 
 public class AtomicReferenceDemo {
+    public static void main(String[] args) {
+        User z3 = new User("z3", 22);
+        User li4 = new User("li4", 25);
+
+        AtomicReference<User> atomicReference = new AtomicReference<>();
+        atomicReference.set(z3);
+
+        System.out.println(atomicReference.compareAndSet(z3, li4) + "\t" + atomicReference.get().toString());
+        System.out.println(atomicReference.compareAndSet(z3, li4) + "\t" + atomicReference.get().toString());
+
+    }
 }
