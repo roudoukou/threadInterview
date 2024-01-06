da585941feaf7d289fd71483f92fdd6420888bfd
肉豆蔻 <1023876294@qq.com>
2024-01-06 10:54:59 +0800

    解决HashMap安全问题

--- src/main/java/com/atguigu/thread/ContainerNotSafeDemo.java
+++ src/main/java/com/atguigu/thread/ContainerNotSafeDemo.java
@@ -1,11 +1,24 @@
 package com.atguigu.thread;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class ContainerNotSafeDemo {
     public static void main(String[] args) {
+        // Map<String, String> map = new HashMap<>(); // java.util.ConcurrentModificationException
+        // Map<String, String> map = new ConcurrentHashMap<>();
+        Map<String, String> map = Collections.synchronizedMap(new HashMap<>());
+        for(int i = 1; i <= 30; i++) {
+            new Thread(() -> {
+                map.put( Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+    }
+
+    private static void setNotSafe() {
         // Set<String> set = new HashSet<>();  // java.util.ConcurrentModificationException
         Set <String> set = new CopyOnWriteArraySet<>();
         for(int i = 1; i <= 30; i++) {
