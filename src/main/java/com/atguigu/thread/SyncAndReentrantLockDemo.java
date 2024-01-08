6c7b282054c337d62b162698b6ff48c2e3c9de24
肉豆蔻 <1023876294@qq.com>
2024-01-08 23:52:41 +0800

    锁绑定多个条件Condition

--- src/main/java/com/atguigu/thread/SyncAndReentrantLockDemo.java
+++ src/main/java/com/atguigu/thread/SyncAndReentrantLockDemo.java
@@ -40,12 +40,94 @@ import java.util.concurrent.locks.ReentrantLock;
  * ...
  * 来 10 轮
  */
+
+class ShareResource {
+    private int number = 1;
+    private Lock lock = new ReentrantLock();
+    Condition c1 = lock.newCondition();
+    Condition c2 = lock.newCondition();
+    Condition c3 = lock.newCondition();
+
+    public void print5() {
+        lock.lock();
+        try {
+            while (number != 1) {
+                try {
+                    c1.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 2;
+            c2.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print10() {
+        lock.lock();
+        try {
+            while (number != 2) {
+                try {
+                    c2.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 3;
+            c3.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print15() {
+        lock.lock();
+        try {
+            while (number != 3) {
+                try {
+                    c3.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            for (int i = 1; i <= 15; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 1;
+            c1.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
 public class SyncAndReentrantLockDemo {
     public static void main(String[] args) {
-        synchronized (new Object()) {
+        ShareResource shareResource = new ShareResource();
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                shareResource.print5();
+            }
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                shareResource.print10();
+            }
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 1; i <= 10; i++) {
+                shareResource.print15();
+            }
+        }, "C").start();
 
-        }
 
-        Lock lock = new ReentrantLock();
     }
 }
