cca4eac168d63930e13ab0ccd11a85babc1a5b1e
肉豆蔻 <1023876294@qq.com>
2024-01-08 14:52:47 +0800

    3-CountDownLatch: 使用CountDownLatch & 枚举类的应用

--- src/main/java/com/atguigu/thread/CountDownLatchDemo.java
+++ src/main/java/com/atguigu/thread/CountDownLatchDemo.java
@@ -1,9 +1,56 @@
 package com.atguigu.thread;
 
+import lombok.Getter;
+
 import java.util.concurrent.CountDownLatch;
+
+enum CountryEnum {
+
+    ONE(1, "齐"), TWO(2, "楚"), THREE(3, "燕"), FOUR(4, "赵"), FIVE(5, "魏"), SIX(6, "韩");
+
+    @Getter
+    private Integer retCode;
+    @Getter
+    private String retMessage;
+
+    CountryEnum(Integer retCode, String retMessage) {
+        this.retCode = retCode;
+        this.retMessage = retMessage;
+    }
+
+    public static CountryEnum forEach_CountryEnum(int index) {
+        for (CountryEnum value : CountryEnum.values()) {
+            if (value.getRetCode() == index) {
+                return value;
+            }
+        }
+
+        return null;
+    }
+}
 
 public class CountDownLatchDemo {
     public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
+        for (int i = 1; i <= 6; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "国, 被灭");
+                countDownLatch.countDown();
+            }, CountryEnum.forEach_CountryEnum(i).getRetMessage()).start();
+        }
+
+        countDownLatch.await();
+
+        System.out.println(Thread.currentThread().getName() + "秦帝国, 一统华夏");
+
+        // 小复习
+        System.out.println();
+        System.out.println(CountryEnum.ONE);
+        System.out.println(CountryEnum.ONE.getRetCode());
+        System.out.println(CountryEnum.ONE.getRetMessage());
+    }
+
+    private static void closeDoor() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(6);
         for (int i = 1; i <= 6; i++) {
             new Thread(() -> {
@@ -38,4 +85,16 @@ main班长最后关门走人
 main班长最后关门走人
 
 3-CountDownLatch: 使用CountDownLatch & 枚举类的应用
+齐国, 被灭
+楚国, 被灭
+燕国, 被灭
+赵国, 被灭
+魏国, 被灭
+韩国, 被灭
+main秦帝国, 一统华夏
+
+ONE
+1
+齐
+
  */
