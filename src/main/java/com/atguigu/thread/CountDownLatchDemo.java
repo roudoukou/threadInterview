2b0ea3494476218d3d0d29ee05666af712963817
肉豆蔻 <1023876294@qq.com>
2024-01-08 14:41:31 +0800

    2-CountDownLatch:使用CountDownLatch

--- src/main/java/com/atguigu/thread/CountDownLatchDemo.java
+++ src/main/java/com/atguigu/thread/CountDownLatchDemo.java
@@ -1,19 +1,25 @@
 package com.atguigu.thread;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountDownLatchDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(6);
         for (int i = 1; i <= 6; i++) {
             new Thread(() -> {
                 System.out.println(Thread.currentThread().getName() + "\t上完晚自习, 离开教室");
+                countDownLatch.countDown();
             }, String.valueOf(i)).start();
         }
+
+        countDownLatch.await();
 
         System.out.println(Thread.currentThread().getName() + "班长最后关门走人");
     }
 }
 
 /*
-未使用 CountDownLatch
+1-CountDownLatch:未使用CountDownLatch
 1	上完晚自习, 离开教室
 5	上完晚自习, 离开教室
 4	上完晚自习, 离开教室
@@ -21,4 +27,15 @@ main班长最后关门走人
 3	上完晚自习, 离开教室
 2	上完晚自习, 离开教室
 6	上完晚自习, 离开教室
+
+2-CountDownLatch:使用CountDownLatch
+2	上完晚自习, 离开教室
+1	上完晚自习, 离开教室
+3	上完晚自习, 离开教室
+5	上完晚自习, 离开教室
+4	上完晚自习, 离开教室
+6	上完晚自习, 离开教室
+main班长最后关门走人
+
+3-CountDownLatch: 使用CountDownLatch & 枚举类的应用
  */
