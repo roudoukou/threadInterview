bc692b43ca7fea22d3869511d3b38c0f08040652
肉豆蔻 <1023876294@qq.com>
2024-01-09 18:45:50 +0800

    Callable接口-多个FutureTask

--- src/main/java/com/atguigu/thread/CallableDemo.java
+++ src/main/java/com/atguigu/thread/CallableDemo.java
@@ -17,19 +17,22 @@ class MyThread implements Callable<Integer> {
 public class CallableDemo {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         // 两个线程 , 一个 main 线程, 一个是 AA futureTask 线程
-        FutureTask<Integer> futureTask = new FutureTask<>(new MyThread());
+        FutureTask<Integer> futureTask1 = new FutureTask<>(new MyThread());
+        FutureTask<Integer> futureTask2 = new FutureTask<>(new MyThread());
 
-        Thread thread = new Thread(futureTask, "AA");
-        thread.start();
+        Thread thread1 = new Thread(futureTask1, "AA");
+        Thread thread2 = new Thread(futureTask2, "BB"); // 需要执行几次Callable, 那就创建几个futureTask
+        thread1.start();
+        thread2.start();
 
         System.out.println(Thread.currentThread().getName());
         int result01 = 100;
 
-        while (!futureTask.isDone()) {
+        while (!futureTask1.isDone()) {
         }
 
         // 要求获得 Callable 线程的计算结果, 如果没有计算完成就要去强求, 会导致堵塞, 值得计算完成
-        int result02 = futureTask.get();
+        int result02 = futureTask1.get();
 
         System.out.println(result01 + result02);
     }
