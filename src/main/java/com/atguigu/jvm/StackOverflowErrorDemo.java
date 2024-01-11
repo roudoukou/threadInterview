76671977709e0df46678ad67b7f57c3637e8ae1e
肉豆蔻 <1023876294@qq.com>
2024-01-12 01:16:15 +0800

    1.java.lang.StackOverflowError

---
+++ src/main/java/com/atguigu/jvm/StackOverflowErrorDemo.java
@@ -0,0 +1,11 @@
+package com.atguigu.jvm;
+
+public class StackOverflowErrorDemo {
+    public static void main(String[] args) {
+        stackOverflowError();
+    }
+
+    private static void stackOverflowError() {
+        stackOverflowError();
+    }
+}
